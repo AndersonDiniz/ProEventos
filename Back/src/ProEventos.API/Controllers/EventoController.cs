dd24e4c35b7f5b6f6eb7c4994ba7ad6cf62351c1
Anderson <acdiniz01@gmail.com>
2021-05-10 16:00:40 -0300

    first commit2

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+       
+        public IEnumerable<Evento> _evento = new Evento[]
+        {
+                new Evento()
+                {
+                    EventoId = 1,
+                    Tema = "ee",
+                    Local = "dsd",
+                    Lote = "asdasd",
+                    QtdPessoas = 2,
+                    DataEvento = DateTime.Now.AddHours(2).ToString("dd/MM/yyyy"),
+                    ImagemUrl = "foto.png"
+                },
+                new Evento()
+                {
+                    EventoId = 2,
+                    Tema = "ee",
+                    Local = "dsd",
+                    Lote = "asdasd",
+                    QtdPessoas = 2,
+                    DataEvento = DateTime.Now.AddHours(2).ToString("dd/MM/yyyy"),
+                    ImagemUrl = "foto.png"
+                }
+            };
+
+            public EventoController(){}
+        
+
+            [HttpGet("{id}")]
+            public IEnumerable<Evento> GetById(int id)
+            {
+                return _evento.Where(e => e.EventoId == id);           
+            }
+
+            [HttpGet]
+            public IEnumerable<Evento> Get(int id)
+            {
+                return _evento;
+                
+            }
+    }
+}
